c01743bcb71533c287de59b6f0717f2833c85347
ylnaangrni <yulina.anggraeni.tif23@polban.ac.id@example.com>
2025-05-22 20:22:59 +0700

    routes untuk riwayat pengajuan TU

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/riwayat-pengajuan-surat', function () {
+    try {
+        return view('surat-view.TU.riwayat-pengajuan-surat');
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
+})->name('riwayat-pengajuan-surat');
+
