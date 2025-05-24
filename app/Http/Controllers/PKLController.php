4bf1ab45743400a33d1c427c9c9825a62f05e8a8
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2025-05-25 00:36:10 +0700

    ✨feat: tambah data kaprodi pada laporan PDF PDPT untuk KP/PKL di controller

--- app/Http/Controllers/PKLController.php
+++ app/Http/Controllers/PKLController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\KpPkl;
+use App\Models\Dosen;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\DB;
 
@@ -24,6 +25,7 @@ class PKLController extends Controller
             'angkatan' => 'required'
         ]);
 
+        $kaprodi = Dosen::where('jabatan_dosen', 'Kaprodi')->first();
         $data = KpPkl::with([
             'mahasiswa.kelas',
             'pembimbing1.dosen',
@@ -41,18 +43,18 @@ class PKLController extends Controller
                 'nim' => $item->mahasiswa->nim,
                 'nama_mhs' => $item->mahasiswa->nama_mhs,
                 'nama_perusahaan' => $item->nama_perusahaan,
-                'pembimbing_1' => $item->pembimbing1->dosen->nama_dosen ?? null,
-                'nidn_pembimbing_1' => $item->pembimbing1->dosen->nidn ?? null,
-                'pembimbing_2' => $item->pembimbing2->dosen->nama_dosen ?? null,
-                'nidn_pembimbing_2' => $item->pembimbing2->dosen->nidn ?? null,
-                'penguji_1' => $item->penguji1->dosen->nama_dosen ?? null,
-                'nidn_penguji_1' => $item->penguji1->dosen->nidn ?? null,
-                'penguji_2' => $item->penguji2->dosen->nama_dosen ?? null,
-                'nidn_penguji_2' => $item->penguji2->dosen->nidn ?? null,
+                'pembimbing_1' => $item->pembimbing1->dosen->nama_dosen ?? '-',
+                'nidn_pembimbing_1' => $item->pembimbing1->dosen->nidn ?? '-',
+                'pembimbing_2' => $item->pembimbing2->dosen->nama_dosen ?? '-',
+                'nidn_pembimbing_2' => $item->pembimbing2->dosen->nidn ?? '-',
+                'penguji_1' => $item->penguji1->dosen->nama_dosen ?? '-',
+                'nidn_penguji_1' => $item->penguji1->dosen->nidn ?? '-',
+                'penguji_2' => $item->penguji2->dosen->nama_dosen ?? '-',
+                'nidn_penguji_2' => $item->penguji2->dosen->nidn ?? '-',
             ];
         });
 
-        $pdf = Pdf::loadView('pkl-view.laporan-pdpt-kp-pkl', compact('data'));
+        $pdf = Pdf::loadView('pkl-view.laporan-pdpt-kp-pkl', compact('data', 'kaprodi'));
         return $pdf->download('laporan_pdpt.pdf');
     }
 
