c7c54093df8d399dd8c8982c977a0dd1ec340ec4
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2025-05-25 00:40:24 +0700

    feat: menambahkan footer pada template laporan PDF PDPT untuk KP/PKL

--- resources/views/pkl-view/laporan-pdpt-kp-pkl.blade.php
+++ resources/views/pkl-view/laporan-pdpt-kp-pkl.blade.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="utf-8">
     <style>
+        @import url('https://fonts.cdnfonts.com/css/times-new-roman');
         body { 
-            font-family: DejaVu Sans, sans-serif; 
+            font-family: 'Times New Roman', serif; 
             font-size: 8px; 
             margin: 20px; 
+            position: relative;
         }
         table { 
-            width: 95%;
+            width: 100%; 
             border-collapse: collapse; 
             margin-top: 10px; 
-            margin-right: 15px;
+            margin-left: 0; 
+            margin-right: 0; 
         }
         th, td { 
             border: 1px solid black; 
@@ -28,9 +31,9 @@
         }
 
         th:nth-child(1), td:nth-child(1) { width: 4%; }
-        th:nth-child(2), td:nth-child(2) { width: 9%; }
-        th:nth-child(3), td:nth-child(3) { width: 12%; }
-        th:nth-child(4), td:nth-child(4) { width: 12%; }
+        th:nth-child(2), td:nth-child(2) { width: 6%; }
+        th:nth-child(3), td:nth-child(3) { width: 9%; }
+        th:nth-child(4), td:nth-child(4) { width: 10%; }
         th:nth-child(5), td:nth-child(5) { width: 10%; }
         th:nth-child(6), td:nth-child(6) { width: 7%; }
         th:nth-child(7), td:nth-child(7) { width: 10%; }
@@ -39,6 +42,33 @@
         th:nth-child(10), td:nth-child(10) { width: 7%; }
         th:nth-child(11), td:nth-child(11) { width: 10%; }
         th:nth-child(12), td:nth-child(12) { width: 7%; }
+        
+        .footer { 
+            margin-top: 20px; 
+            position: absolute; 
+            right: 0; 
+            width: auto; 
+        }
+        .footer-table { 
+            border-collapse: collapse; 
+            border: none; 
+        }
+        .footer-table td { 
+            border: none; 
+            padding: 0; 
+            text-align: left; 
+            background-color: white;
+        }
+        .footer-table p { 
+            margin: 0; 
+            padding-top: 0pt; 
+            padding-bottom: 6pt; 
+            line-height: 1.5; 
+            font-size: 8pt; 
+        }
+        .signature-gap { 
+            height: 80px; 
+        }
         
         @page { 
             size: A4; 
@@ -90,5 +120,25 @@
             @endforeach
         </tbody>
     </table>
+
+    <div class="footer">
+        <table class="footer-table">
+            <tr>
+                <td><p>Program Studi Teknik Informatika {{ $prodi->nama_prodi ?? '' }}</p></td>
+            </tr>
+            <tr>
+                <td><p>Ketua,</p></td>
+            </tr>
+            <tr>
+                <td class="signature-gap"></td>
+            </tr>
+            <tr>
+                <td><p>{{ $kaprodi->nama_dosen ?? 'Nama Dosen' }}</p></td>
+            </tr>
+            <tr>
+                <td><p>NIP {{ $kaprodi->nip ?? '000000000' }}</p></td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
