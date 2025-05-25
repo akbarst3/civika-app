5a57754cf41053ade52e39f70f084f852ce3cef3
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2025-05-25 15:03:40 +0700

    :sparkles: Menambahkan fitur impor data mahasiswa dari file Excel

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Imports\DataMahasiswaImport;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Routing\Controller;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function import(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls',
+            'angkatan' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        try {
+            Excel::import(new DataMahasiswaImport($request->angkatan), $request->file('file'));
+            return redirect()->back()->with('success', 'Data mahasiswa berhasil diimpor.');
+        } catch (\Exception $e) {
+            Log::error('Error importing data: ' . $e->getMessage());
+            return redirect()->back()->with('error', 'Gagal mengimpor data mahasiswa: ' . $e->getMessage());
+        }
+    }
 }
