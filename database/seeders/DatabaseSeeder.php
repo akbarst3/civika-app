e6898764207e80ecfa8146cedd263ba90df4f84f
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2025-06-19 06:39:09 +0700

    :hammer: Update DatabaseSeeder and DosenSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,13 +24,13 @@ class DatabaseSeeder extends Seeder
             KelasSeeder::class,
             DosenSeeder::class,
             MahasiswaSeeder::class,
+            TugasAkhirSeeder::class,
 //            TugasAkhirSeeder::class,
 //            MembimbingTASeeder::class,
 //            MengujiTASeeder::class,
             KpPklSeeder::class,
             MembimbingKpPklSeeder::class,
             MengujiKpPklSeeder::class,
-            TugasAkhirSeeder::class,
         ]);
     }
 }
